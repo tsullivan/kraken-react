b0c1fbe2285b55ac1f14de345cfdc903e305c3d3
Timothy Sullivan <tsullivan@paypal.com>
2014-06-22 23:34:22 -0700

    add comments to list

--- controllers/index.js
+++ controllers/index.js
@@ -3,21 +3,20 @@
 var CommentModel = require('../models/comment');
 
 module.exports = function (router) {
-	var model = [
-		new CommentModel({"author": "Pete Hunt", "text": "This is one comment"}),
-		new CommentModel({"author": "Jordan Walke", "text": "This is *another* comment"})
-	];
+
+	var comments = [];
 
 	router.get('/', function (req, res) {
 		res.render('index');
 	});
 
 	router.get('/comments.json', function (req, res) {
-		res.json(model);
+		res.json(comments);
 	});
 
 	router.post('/comments.json', function (req, res) {
-		res.json({});
+		comments[comments.length] = new CommentModel({'author': req.body.author, 'text': req.body.text});
+		res.json(comments);
 	});
 
 };
