31c27e02f8307093beec56cc12ed7bbb6532e157
Timothy Sullivan <tsullivan@paypal.com>
2014-06-22 22:36:28 -0700

    test form post

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,16 @@
 'use strict';
 
-
 var IndexModel = require('../models/index');
 
-
 module.exports = function (router) {
+	var model = new IndexModel();
 
-    var model = new IndexModel();
+	router.get('/', function (req, res) {
+		res.render('index', model);
+	});
 
-
-    router.get('/', function (req, res) {
-        
-        res.render('index', model);
-        
-    });
+	router.post('/', function (req, res) {
+		res.render('index', model);
+	});
 
 };
